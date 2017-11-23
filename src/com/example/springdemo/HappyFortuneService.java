b73817946d40271f8322dc3a43cace7fa4db662e
Fouchimi <ousmanefouchimi@gmail.com>
2017-11-22 21:01:40 -0700

    Added randomfortunes

--- src/com/example/springdemo/HappyFortuneService.java
+++ src/com/example/springdemo/HappyFortuneService.java
@@ -2,9 +2,17 @@ package com.example.springdemo;
 
 public class HappyFortuneService implements FortuneService {
 
+	private String[] fortunes = {"Today is your lucky day", 
+			"You've earned points to move to the next step", 
+			"Awesome coding, congrats !!"};
+	
 	@Override
 	public String getFortune() {
-		return "Today is your lucky day !";
+		
+		int randNumber = (int) (Math.random() * (fortunes.length)) % fortunes.length;
+		System.out.println("Index : " + randNumber);
+		
+		return fortunes[randNumber];
 	}
 
 }
