40f42298f940e6146e8ce972c68ab5d02555ba1a
Fouchimi <ousmanefouchimi@gmail.com>
2017-11-22 18:06:14 -0700

    Inject HappyFortuneService

--- src/com/example/springdemo/BaseballCoach.java
+++ src/com/example/springdemo/BaseballCoach.java
@@ -2,10 +2,24 @@ package com.example.springdemo;
 
 public class BaseballCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	//Define constructor for fortune service
+	public BaseballCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		// TODO Auto-generated method stub
 		return "Spend 30 minutes on batting practice !";
+	}
+
+	@Override
+	public String getDailyFortune() {
+	
+		//use fortuneService to get a fortune
+		return fortuneService.getFortune();
 	}
 
 	
